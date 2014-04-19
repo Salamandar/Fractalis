5bc3435a222be9d4557c215ef1cfc8f302d52ecc
Félix Piédallu <felix@piedallu.me>
2014-04-19 23:27:45 +0200

    BIG modifs Changé les noms génériques des variables de Données. Rajouté la possibilité de fonctions personnalisables. Pour le coup, le calcul est censé fonctionner : c'est la fonction convergence à laquelle on envoie la position de calcul.

--- u4-fonctions.cpp
+++ u4-fonctions.cpp
@@ -9,19 +9,65 @@ using namespace std;
 #include "u1-interface.h"
 #include "u4-fonctions.h"
 
-// Definition des donnees fonctionnelles du projet - structure globale de variables
 struct Donnees gDonnees;
+
+// Donne un rang de convergence pour un point du plan complexe, à utiliser pour déterminer couleur d'affichage
+int convergence(std::complex<double> position){
+    pointeurFct fonction = retourne_fonction();
+    int rangDivergence = -1;
+    int i=0;
+    std::complex<double> terme = position;
+    while ((std::abs(terme = fonction(position,terme)))>gDonnees.moduleMax){
+        i++;
+    }
+}
+
+
+// Pointe vers les fonctions suivantes en fonction de la fractale choisie
+pointeurFct retourne_fonction() {
+    switch(gDonnees.Fractale){
+        case MANDELBROT:
+            return mandelbrot;
+            break;
+        case JULIA:
+            return julia;
+            break;
+        case PERSONNA:
+            return personna;
+            break;
+    }
+}
+
+
+std::complex<double> mandelbrot(std::complex<double> positZero, std::complex<double> position){
+    return position*position + gDonnees.C;
+}
+std::complex<double> julia     (std::complex<double> positZero, std::complex<double> position){
+    return position*position + positZero;
+}
+std::complex<double> personna  (std::complex<double> constante, std::complex<double> position){
+    return (0,0);
+}
+/*  À voir. Ce sera la fonction personnalisable, mais implémentée seulement quand TOUT le reste fonctionnera.
+    J'aurai besoin de maîtriser le parsage de fonction mathématique, puis l'allocation dynamique de fonction.
+    Ça sera extrêmement moche, donc si Nils a une idée elle est la bienvenue.
+*/
+
+
+// Fonctions de test.
+
+
+
+
 
 void InitialiserDonnees() {
     gDonnees.Fractale = FRACT_INIT;
-    gDonnees.N = N_INIT;
-    gDonnees.Z = Z_INIT;
+    gDonnees.rangMax  = RANGMAX_INIT;
+    gDonnees.moduleMax= MODULEMAX_INIT;
     gDonnees.C = C_INIT;
     gDonnees.ig= IG_INIT;
     gDonnees.sd= SD_INIT;
 }
-
-
 
 // Cette procedure permet une attente de x secondes, x peut etre en secondes mais aussi en flottant par exemple : 0.1 s
 void Attente ( double Seconds ) {
@@ -29,7 +75,4 @@ void Attente ( double Seconds ) {
     Endwait = (int) (clock () + Seconds * CLOCKS_PER_SEC);
     while (clock() < Endwait);
 }
-
-
-
 
--- u4-fonctions.h
+++ u4-fonctions.h
@@ -6,8 +6,8 @@
 #define PROFONDEUR_MAX 10000
 // Données utilisateurs initiales :
 #define FRACT_INIT MANDELBROT
-#define N_INIT  250
-#define Z_INIT  1
+#define RANGMAX_INIT    250
+#define MODULEMAX_INIT  1
 #define C_INIT  (1,1)
 #define IG_INIT (1,1)
 #define SD_INIT (2,2)
@@ -15,55 +15,43 @@
 #define HAUTEUR 100
 #define LARGEUR 100
 
-// Structure de Pixel
-struct Pixel {
-    std::complex<double> z;  // Coordonnées dans le plan std::complexe
-    int n;      // rang de divergence
-};
+typedef std::complex<double> (*pointeurFct)(std::complex<double>, std::complex<double>);
+    // Type d'une fonction qui renvoie un pointeur vers le type de fractale
 
 enum fractype {
     MANDELBROT,
-    JULIA
+    JULIA,
+    PERSONNA
 };
 
+// Structure de Pixel
+struct Pixel {
+    std::complex<double> z; // Coordonnées dans le plan std::complexe
+    int n;  // rang de divergence
+};
 
 // Structure des paramètres utilisateur
 struct Donnees {
-    enum fractype Fractale; // Type de fractales choisie (Type énuméré)
-    int N;                  // Rang maximal de convergence
-    int Z;                  // Module de convergence (détermination de la convergence on non de la fonction)
-    std::complex<double> C;          // Constante de calcul
-    std::complex<double> ig;         // Coordonnées du point inférieur gauche
-    std::complex<double> sd;         // Coordonnées du point haut droit
+    enum fractype Fractale;     // Type de fractales choisie (Type énuméré)
+    int     rangMax;            // Rang maximal de convergence
+    double  moduleMax;          // Module de convergence (détermination de la convergence on non de la fonction)
+    std::complex<double> C;     // Constante de calcul
+    std::complex<double> ig;    // Coordonnées du point inférieur gauche
+    std::complex<double> sd;    // Coordonnées du point haut droit
     struct Pixel Tab[HAUTEUR][LARGEUR]; // Matrice des pixels de l'image.
 };
 
-
-extern struct Donnees gDonnees;
-
 void InitialiserDonnees() ;
-
-
-void Attente(double Seconds);
-
 
 //int couleurN(int N);    // Sort une couleur à partir d'un N. Géré par Nils ?
 
-int convergence(std::complex<double> position); // Donne un rang de convergence pour un point du plan complexe,
+int convergence(std::complex<double> position); // Donne un rang de convergence pour un point du plan complexe
+
+pointeurFct retourne_fonction();    // Pointe vers les fonctions suivantes en fonction de la fractale choisie
+
+std::complex<double> mandelbrot(std::complex<double> constante, std::complex<double> position);
+std::complex<double> julia     (std::complex<double> constante, std::complex<double> position);
+std::complex<double> personna  (std::complex<double> constante, std::complex<double> position);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif // _u4_fonctions_h
+#endif
