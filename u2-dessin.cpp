e82fd49d7c261f315159ad7468376f7463121c67
Benz0X <nils.exibard@gmail.com>
2014-05-21 23:10:45 +0200

    FIXEEEEEED

--- u2-dessin.cpp
+++ u2-dessin.cpp
@@ -13,7 +13,7 @@ void ZoneDessinInitialisation(Fl_Widget* widget, void* data)
 {
     fl_color(FL_BLACK);
     fl_rectf(X_ZONE, Y_ZONE, L_ZONE, H_ZONE);
-    // On initialise la gestion de l'affichage de la fractale seulement quand la fenêtre est correctement créée
+     On initialise la gestion de l'affichage de la fractale seulement quand la fenêtre est correctement créée
     gTests.dessin=true;
     Fl::add_timeout(0, gestionAffichage_iter, NULL );
 }
@@ -81,25 +81,25 @@ void gestionAffichage_iter(void*)
         couleursRGB(gDonnees.color1,gDonnees.color2,gDonnees.color3,gDonnees.rangColor1,gDonnees.rangColor2,gDonnees.rangColor3,tabDegrade);
         for(int j=0; j<H_ZONE; j++)
         {
-            for (int i = 0; i < L_ZONE; i=i+3)
+            for (int i = 0; i < L_ZONE; i=i+1)
             {
                 if (gDonnees.Tab[i][j].n==-1 )
                 {
-                    gDonnees.buffer[i+L_ZONE*j]=0;
-                    gDonnees.buffer[i+L_ZONE*j+1]=0;
-                    gDonnees.buffer[i+L_ZONE*j+2]=0;
+                    //gDonnees.buffer[i+L_ZONE*j]=0;
+                    //gDonnees.buffer[i+L_ZONE*j+1]=0;
+                    //gDonnees.buffer[i+L_ZONE*j+2]=0;
                 }
 
                 else
                 {
 
-                    gDonnees.buffer[i+L_ZONE*j]=tabDegrade[gDonnees.Tab[i][j].n][0];
-                    gDonnees.buffer[i+L_ZONE*j+1]=tabDegrade[gDonnees.Tab[i][j].n][1];
-                    gDonnees.buffer[i+L_ZONE*j+2]=tabDegrade[gDonnees.Tab[i][j].n][2];
+                    gDonnees.buffer[3*i+3*L_ZONE*j]=tabDegrade[gDonnees.Tab[i][j].n][0];
+                    gDonnees.buffer[3*i+3*L_ZONE*j+1]=tabDegrade[gDonnees.Tab[i][j].n][1];
+                    gDonnees.buffer[3*i+3*L_ZONE*j+2]=tabDegrade[gDonnees.Tab[i][j].n][2];
                 }
+
             }
-            //cout<<tabDegrade[gDonnees.Tab[100][j].n][0]<<" "<<tabDegrade[gDonnees.Tab[100][j].n][1]<<" "<<tabDegrade[gDonnees.Tab[100][j].n][2]<<endl;
-            cout<<j<<endl;
+            //for(int i=0;i<60;i=i+3)
         }
         gTests.calccouleurs=0;
         gTests.calcul=0;
